56dc4f921f2e3ac711b18f5f09b0c9d849581b37
ofekbenshlush <116751278+ofekbenshlush@users.noreply.github.com>
2026-01-16 16:56:15 +0200

    Update and rename design.v to design.v

--- Day_1 - Mux 2:1/design.v
+++ Day_1 - Mux 2:1/design.v
@@ -1,8 +1,8 @@
-module mux 2to1(
+module mux2to1(
   input wire [7:0] a,
   input wire [7:0] b,
   input wire sel,
   output wire [7:0] y
 );
-  assign y = (sel ==1'b0) ? a ; b;
+  assign y = (sel ==1'b0) ? a : b;
 endmodule
