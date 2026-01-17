b371cd27176a37ad9545e3068623dfdbdac69aa7
ofekbenshlush <116751278+ofekbenshlush@users.noreply.github.com>
2026-01-17 18:56:44 +0200

    Create design.v

---
+++ Day_2 - DFF/design.v
@@ -0,0 +1,6 @@
+input     logic      clk,
+input     logic      reset,
+input     logic      d_i,         -> D input to the flop
+output    logic      q_norst_o,   -> Q output from non-resettable flop
+output    logic      q_syncrst_o, -> Q output from flop using synchronous reset
+output    logic      q_asyncrst_o -> Q output from flop using asynchrnoous reset
