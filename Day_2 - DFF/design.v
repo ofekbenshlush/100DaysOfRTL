a7ee05fd1bf68294e63a0edc5a436732eb298578
ofekbenshlush <116751278+ofekbenshlush@users.noreply.github.com>
2026-01-18 17:27:04 +0200

    Update design.v

--- Day_2 - DFF/design.v
+++ Day_2 - DFF/design.v
@@ -1,6 +1,24 @@
-input     logic      clk,
-input     logic      reset,
-input     logic      d_i,         -> D input to the flop
-output    logic      q_norst_o,   -> Q output from non-resettable flop
-output    logic      q_syncrst_o, -> Q output from flop using synchronous reset
-output    logic      q_asyncrst_o -> Q output from flop using asynchrnoous reset
+module DFF (
+  input logic d,
+  input logic clk,
+  input logic rst,
+  output logic q,
+  output logic rst_sync_q,
+  output logic rst_async_q
+);
+  //Reset is not active.
+  always_ff @(posedge clk)
+    q <= d;
+  //Sync reset - in case reset is on, waiting to the next clock to reset.
+  always_ff @(posedge clk) 
+    if (rst)
+      rst_sync_q <= 1'b0;
+  	else
+      rst_sync_q <= d;
+  //Async reset - will active immediatly when a reset is on without waiting to the next clock.
+  always_ff @(posedge clk or posedge rst)
+    if (rst)
+      rst_async_q <= 1'b0;
+  	else 
+      rst_async_q <= d;
+endmodule 
