98efd806d23226ae6d03e2bc977510a02757d31d
ofekbenshlush <116751278+ofekbenshlush@users.noreply.github.com>
2026-02-16 13:44:02 +0200

    Create design.sv

---
+++ Day_3/design.sv
@@ -0,0 +1,17 @@
+module(
+  input logic clk,
+  input logic reset,
+  input logic d_i,
+  output logic rising_edge_o,
+  output logic falling_edge_o
+);
+  logic d_ff;
+  always_ff @(posedge clk or posedge reser)
+    if(reset)
+      d_ff <= 1'b0;
+    else
+      d_ff <= d_i;
+  assign rising_edge_o = ~d_ff & d_i;
+  assign falling_edge_o = d_ff & ~d_i;
+
+endmodule
