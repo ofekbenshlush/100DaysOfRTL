e80155ebe23f4780f274a5ff4173a068d702a12a
ofekbenshlush <116751278+ofekbenshlush@users.noreply.github.com>
2026-02-25 15:13:36 +0200

    Add testbench for edge_detector module
    final TB design

---
+++ Day_3/testbanch_day3.sv
@@ -0,0 +1,40 @@
+`timescale 1ns/1ps
+module edge_detector_tb();
+  logic clk;
+  logic reset;
+  logic d_i;
+  logic rising_edge_o;
+  logic falling_edge_o;
+  
+  edge_detector dut(
+    .clk(clk),
+    .reset(reset),
+    .d_i(d_i),
+    .rising_edge_o(rising_edge_o),
+    .falling_edge_o(falling_edge_o)
+  );
+  always #5 clk =~clk;
+    initial begin
+      clk = 0;
+      reset = 1'b1;
+      d_i = 1'b0;
+
+      #20 reset = 1'b0;
+
+      #10 d_i = 1'b1;
+      #20 d_i = 1'b0;
+
+      #15 d_i = 1'b1;
+      #5  d_i = 1'b0; 
+
+      #30;
+      $display("Simulation complete.");
+      $finish;
+    end
+
+    initial begin
+      $dumpfile("dump.vcd");
+      $dumpvars(0, edge_detector_tb);
+    end
+
+ endmodule
