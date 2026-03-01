3f1e7c80e8db9af78f5eb930ea0da04771a538fb
ofekbenshlush <116751278+ofekbenshlush@users.noreply.github.com>
2026-03-01 11:59:25 +0200

    Refactor DFF module for improved readability

--- Day_2 - DFF/design.v
+++ Day_2 - DFF/design.v
@@ -1,24 +1,24 @@
 module DFF (
-  input logic d,
-  input logic clk,
-  input logic rst,
-  output logic q,
-  output logic rst_sync_q,
-  output logic rst_async_q
+  input  logic d,
+  input  logic clk,
+  input  logic rst,
+  output logic q,
+  output logic rst_sync_q,
+  output logic rst_async_q
 );
-  //Reset is not active.
-  always_ff @(posedge clk)
-    q <= d;
-  //Sync reset - in case reset is on, waiting to the next clock to reset.
-  always_ff @(posedge clk) 
-    if (rst)
-      rst_sync_q <= 1'b0;
-  	else
-      rst_sync_q <= d;
-  //Async reset - will active immediatly when a reset is on without waiting to the next clock.
-  always_ff @(posedge clk or posedge rst)
-    if (rst)
-      rst_async_q <= 1'b0;
-  	else 
-      rst_async_q <= d;
-endmodule 
+
+  always_ff @(posedge clk)
+    q <= d;
+  always_ff @(posedge clk) begin
+    if (rst)
+      rst_sync_q <= 1'b0;
+    else
+      rst_sync_q <= d;
+  end
+  always_ff @(posedge clk or posedge rst) begin
+    if (rst)
+      rst_async_q <= 1'b0;
+    else
+      rst_async_q <= d;
+  end
+endmodule
